09184c9edf387f6b664233daf103b17dc96c84ce
Ricardo0331 <ricardose0419@gmail.com>
2023-10-28 14:17:56 -0500

    implemented API endpoints for Usage metrics and added error handlers

--- src/app.js
+++ src/app.js
@@ -41,5 +41,56 @@ app.put("/urls/:urlId", (req, res) => {
     res.json({ data: foundUrl });
 }); 
 
+//create a usage metric when a URL is accessed 
+app.get("/urls/:urlId", (req, res) => {
+    const { urlId } = req.params;
+    const foundUrl = urls.find((url) => url.id === Number(urlId));
+    const newUseId = uses.length + 1; // assign a new ID
+    const newUse = { id: newUseId, urlId: Number(urlId), time: Date.now() };
+
+    uses.push(newUse); // add to the uses array
+    res.json({ data: foundUrl });
+});
+
+
+//GET route to retrieve usage metric by its ID 
+app.get("/uses/:useId", (req, res) => {
+    const { useId } = req.params;
+    const foundUse = uses.find((use) => use.id === Number(useId));
+
+    res.json({ data: foundUse }); 
+});
+
+
+// GET route to retrieve a list of all usage metrics
+app.get("/uses", (req, res) => {
+    res.json({ data: uses }); 
+});
+
+
+//DELETE route to delete a usage metric by its ID
+app.delete("/uses/:useId", (req, res) => {
+    const { useId } = req.params
+    const index = uses.findIndex((use) => use.id === Number(useId));
+
+    uses.splice(index, 1) // remove the usage metric
+    res.sendStatus(204); // no content
+});
+
+
+//ERROR HANDLING
+
+//Middleware for 404 errors
+app.use((req, res, next) => {
+    res.status(404).json({ error: "Resource not found" });
+});
+
+
+//405 for DELETE on /urls/:urlId
+app.delete("/url/:urlId", (req, res) => {
+    res.status(405).json({ error: "Method not allowed" }); 
+});
+
+
 //xport the app
 module.exports = app;
